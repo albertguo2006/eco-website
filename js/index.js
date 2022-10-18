7a9a96820611d9d36cbc1df0e2fb02acb494ba48
Albert Guo <albertguo2006@gmail.com>
2022-10-17 19:08:59 -0700

    style changes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
-$(window).scroll(function()
+$(document).ready(function()
 {
-    if ($(this).scrollTop() > 10)
-    {
-        $('.display-none').stop().fadeIn(800);
-    }
-    else
-    {
-        $('.display-none').stop().fadeOut(800);
-    };
+    $('.display-none').stop().fadeIn(1200);
 });
